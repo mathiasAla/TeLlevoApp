73ff31e87540e0014e40620161d29284fdbcc549
VIsantibanezz <139240244+VIsantibanezz@users.noreply.github.com>
2024-09-08 22:48:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TellevoApp',
+  webDir: 'www'
+};
+
+export default config;
